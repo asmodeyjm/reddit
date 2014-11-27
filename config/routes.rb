59f2ee9477fda71162dca8ac374a6723d6bc4759
Marco Jovovich <mtjovovich@gmail.com>
2014-11-26 19:07:33 -0500

    created post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  # get 'posts/index'
+  # get 'posts/show'
+  # get 'posts/new'
+  # get 'posts/edit'
+  resources :posts
 
-  get 'welcome/about'
+
+
+  # get 'welcome/index'
+  # get 'welcome/about'
+
+  get 'about' => 'welcome#about'
+
+
 
   root to: 'welcome#index'
 
